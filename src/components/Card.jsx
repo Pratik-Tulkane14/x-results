494672470173159e39fa47b6679c552977c4db75
Pratik-Tulkane14 <pratiktulkane12345@gmail.com>
2025-03-24 18:51:13 +0530

    add props validation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,31 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Card = ({ icon, color, category, marks }) => {
-  console.log(icon, category, marks);
   return (
-    <>
-      <div className="card">
-        <div className="card-left">
-            {icon}
-          <p>{category}</p>
-        </div>
-        <div className="card-right">
-          <p>
-            <span className="optain-marks">{marks}</span>
-            /100
-          </p>
-        </div>
+    <div className="card">
+      <div className="card-left">
+        {icon}
+        <p>{category}</p>
       </div>
-    </>
+      <div className="card-right">
+        <p>
+          <span className="optain-marks">{marks}</span>
+          /100
+        </p>
+      </div>
+    </div>
   );
+};
+
+Card.propTypes = {
+  icon: PropTypes.element.isRequired,
+  color: PropTypes.string,
+  category: PropTypes.string.isRequired,
+  marks: PropTypes.number.isRequired,
+};
+
+Card.defaultProps = {
+  color: "#000000",
 };
 export default Card;
